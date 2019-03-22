f4bd80cf16adf5acc424d38ba35e89160d50b8b0
daniel-wesley <danwfm@gmail.com>
2019-03-21 21:41:28 -0300

    criaçao das estruturas do projeto

---
+++ app/pessoa/pessoa.js
@@ -0,0 +1,11 @@
+function primeiraFuncao(){
+    alert('ola Daniel');
+};
+
+function somaNumeros(){
+    var num1 = prompt('Digite o primeiro número');
+    var num2 = prompt('Digite o segundo número');
+    var resultado = parseInt(num1) + parseInt(num2);
+   
+    alert(resultado);
+};
