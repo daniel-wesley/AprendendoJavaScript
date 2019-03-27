ce25c67d2dffa01178518bc8753fbf88f6791c92
daniel-wesley <danwfm@gmail.com>
2019-03-26 21:35:23 -0300

    Acrescentamos imagens locais e altilizamos eventos das tags.

--- app/pessoa/pessoa.js
+++ app/pessoa/pessoa.js
@@ -1,11 +1,26 @@
 function primeiraFuncao(){
     alert('ola Daniel');
-};
+}
 
 function somaNumeros(){
     var num1 = prompt('Digite o primeiro número');
     var num2 = prompt('Digite o segundo número');
     var resultado = parseInt(num1) + parseInt(num2);
-   
+    
     alert(resultado);
-};
+}
+
+function saiuDaArea(){
+    alert('Você saiu da Área');
+}
+
+function voceArrastou(){
+    alert('Não arraste o botão');
+}
+
+function enviarNomeParaOTitulo(tag){
+    var tituloOla = document.getElementById('tituloOla');
+
+    //Ola               = Ola                 + " " + Valor que vier de fora
+    tituloOla.innerText = tituloOla.innerText + " " + tag.value;
+}
